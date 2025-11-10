a9c606bbf019543d6856a336346cd8b13ea19812
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-10 23:54:58 +0530

    again commit

--- BASIC/Factorial.java
+++ BASIC/Factorial.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Factorial {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    try {
+
+    try (Scanner sc = new Scanner(System.in)) {
       System.out.print("Enter a non-negative integer: ");
+
       if (!sc.hasNextInt()) {
         System.out.println("Error: input is not an integer.");
         return;
@@ -23,8 +24,6 @@ public class Factorial {
       }
 
       System.out.println(n + "! = " + fact);
-    } finally {
-      sc.close();
     }
   }
 }
